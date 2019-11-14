57a2efe5581331eec6919db41ee50b2d3ca04a14
czvr0 <chri656v@edu.sde.dk>
2019-11-14 12:58:50 +0100

    Added start_price

--- model/productModel.php
+++ model/productModel.php
@@ -86,14 +86,12 @@ class ProductModel extends \MODEL\BASE\Model {
      */
     public function createProduct(string $name, string $description, float $price) : array {
         
-        $database = $this->database->query("INSERT INTO products (name, description, price) VALUES (:name, :description, :price)", ["name" => $name, "description" => $description, "price" => $price]);
+        $database = $this->database->query("INSERT INTO products (name, description, price, start_price) VALUES (:name, :description, :price, :price)", ["name" => $name, "description" => $description, "price" => $price]);
 
         $status = ($database->affectedRows() > 0);
         $id = $database->getLastAutoID();
 
-        return ["id" => $id, "status" => $status];
-        
-        //return ($this->database->query("INSERT INTO products (name, description, price) VALUES (:name, :description, :price)", ["name" => $name, "description" => $description, "price" => $price])->affectedRows() > 0);
+        return ["id" => $id, "status" => $status];    
     }
 
     /**
